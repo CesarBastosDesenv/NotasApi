670e4f1cdfd488968c5e0bbda77fc60816b933aa
CesarBastosDesenv <csouza.trabalho@gmail.com>
2025-06-21 05:10:53 -0300

    Criação da Migrations

--- src/Nota.Api/Program.cs
+++ src/Nota.Api/Program.cs
@@ -1,11 +1,28 @@
+//using Nota.application.Interfaces;
+//using Nota.application.Services;
+using Nota.Infra.Data.Context;
+using Nota.Infra.Data.Interfaces;
+using Nota.Infra.Data.Repositories;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers().AddJsonOptions(options =>
+{
+    options.JsonSerializerOptions.ReferenceHandler = System.Text.Json.Serialization.ReferenceHandler.IgnoreCycles;
+});
+;
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+var connectionString = builder.Configuration.GetConnectionString("ApiConnection");
+
+builder.Services.AddDbContext<ApiContext>(Options => {
+                      Options.UseSqlServer(connectionString, dbOpts => dbOpts.MigrationsAssembly(typeof(Program).Assembly.FullName));
+                   });
 
 var app = builder.Build();
 
